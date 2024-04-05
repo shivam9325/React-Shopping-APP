b37016ae94aa13f0e7ceaa5cedd0b7ba3ab16a55
shivam9325 <97913631+shivam9325@users.noreply.github.com>
2024-04-05 16:51:02 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import NavbarBs from "./components/NavbarBs";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+
+function App() {
+  return (
+    <>
+      <ShoppingCartProvider>
+        <NavbarBs />
+        <Container className="mb-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Store" element={<Store />} />
+            <Route path="/About" element={<About />} />
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
+    </>
+  );
+}
+
+export default App;
